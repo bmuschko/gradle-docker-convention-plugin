51e39e0cf61d43cd02605a3fb1a3986bc05ee07a
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-03-14 18:08:01 -0600

    Add missing files

--- plugin/src/main/groovy/com/bmuschko/gradle/docker/DockerNodeJsApplicationPlugin.groovy
+++ plugin/src/main/groovy/com/bmuschko/gradle/docker/DockerNodeJsApplicationPlugin.groovy
@@ -1,16 +1,14 @@
 package com.bmuschko.gradle.docker
 
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.api.tasks.Sync
-import com.bmuschko.gradle.docker.DockerRemoteApiPlugin
-import com.bmuschko.gradle.docker.DockerExtension
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Sync
 
 class DockerNodeJsApplicationPlugin implements Plugin<Project> {
-    static final String NODE_JS_APPLICATION_EXTENSION_NAME = 'nodeJsApplication'
+    public static final String NODE_JS_APPLICATION_EXTENSION_NAME = 'nodeJsApplication'
     public static final String DOCKERFILE_TASK_NAME = 'createDockerfile'
     public static final String SYNC_DIST_RESOURCES_TASK_NAME = 'syncNodeFiles'
     public static final String BUILD_IMAGE_TASK_NAME = 'buildImage'
@@ -23,8 +21,8 @@ class DockerNodeJsApplicationPlugin implements Plugin<Project> {
         DockerExtension dockerExtension = project.extensions.getByType(DockerExtension)
         DockerNodeJsApplication dockerNodeJsApplication = dockerExtension.extensions.create(NODE_JS_APPLICATION_EXTENSION_NAME, DockerNodeJsApplication)
         
-        Dockerfile createDockerfileTask = createDockerfileTask(project)
-        Sync distSyncTask = createDistCopyResourcesTask(project, createDockerfileTask, dockerNodeJsApplication)
+        Dockerfile createDockerfileTask = createDockerfileTask(project, dockerNodeJsApplication)
+        Sync distSyncTask = createDistSyncResourcesTask(project, createDockerfileTask)
         createDockerfileTask.dependsOn distSyncTask
         DockerBuildImage dockerBuildImageTask = createBuildImageTask(project, createDockerfileTask, dockerNodeJsApplication)
         createPushImageTask(project, dockerBuildImageTask)
@@ -33,18 +31,20 @@ class DockerNodeJsApplicationPlugin implements Plugin<Project> {
     private Dockerfile createDockerfileTask(Project project, DockerNodeJsApplication dockerNodeJsApplication) {
         project.task(DOCKERFILE_TASK_NAME, type: Dockerfile) {
             description = 'Creates the Docker image for the Node.js application.'
+            group = 'Docker'
             from { dockerNodeJsApplication.baseImage }
             copyFile('package*.json', './')
             copyFile('index.js', '/index.js')
             runCommand('npm install')
             entryPoint('node', 'index.js')
-            exposePort { dockerJavaApplication.ports }
+            exposePort { dockerNodeJsApplication.ports }
         }
     }
     
     private Sync createDistSyncResourcesTask(Project project, Dockerfile createDockerfileTask) {
         project.task(SYNC_DIST_RESOURCES_TASK_NAME, type: Sync) {
             description = "Copies the distribution resources to a temporary directory for image creation."
+            group = 'Docker'
             from('.') {
                 include 'package*.json'
             }
@@ -55,7 +55,8 @@ class DockerNodeJsApplicationPlugin implements Plugin<Project> {
     
     private DockerBuildImage createBuildImageTask(Project project, Dockerfile createDockerfileTask, DockerNodeJsApplication dockerNodeJsApplication) {
         project.task(BUILD_IMAGE_TASK_NAME, type: DockerBuildImage) {
-            description = 'Builds the Docker image for the Java application.'
+            description = 'Builds the Docker image for the Node.js application.'
+            group = 'Docker'
             dependsOn createDockerfileTask
             conventionMapping.inputDir = { createDockerfileTask.destFile.parentFile }
             conventionMapping.tag = { dockerNodeJsApplication.getTag() }
@@ -65,6 +66,7 @@ class DockerNodeJsApplicationPlugin implements Plugin<Project> {
     private void createPushImageTask(Project project, DockerBuildImage dockerBuildImageTask) {
         project.task(PUSH_IMAGE_TASK_NAME, type: DockerPushImage) {
             description = 'Pushes created Docker image to the repository.'
+            group = 'Docker'
             dependsOn dockerBuildImageTask
             conventionMapping.imageName = { dockerBuildImageTask.getTag() }
         }
